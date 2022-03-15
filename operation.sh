a514d9230c0a06048425237e5711502f90a68ca4
Vincenthuang282 <78361186+Vincenthuang282@users.noreply.github.com>
2022-03-15 18:03:56 +0800

    Update operation.sh

--- operation.sh
+++ operation.sh
@@ -3,4 +3,4 @@ sudo yum -y install python3
 sudo yum -y install python3-pip
 sudo pip3 install --upgrade pip
 pip3 install -r requirements.txt 
-
+python3 /home/vincenthuang282/StockAndTelegram/main.py
