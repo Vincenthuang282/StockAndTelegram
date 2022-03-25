f710b1e8cf6ddfb7a2d2efc9dd7ccab275685f98
Vincent <Vincent>
2022-03-25 09:44:27 +0000

    change the data_refresh

--- data_refresh.sh
+++ data_refresh.sh
@@ -3,3 +3,6 @@
 python3 /home/vincenthuang282/StockAndTelegram/Fetch_Data_to_database.py 
 rm /home/vincenthuang282/StockAndTelegram/result_file/*
 python3 /home/vincenthuang282/StockAndTelegram/data_analyist.py 
+git add *
+git commit -m "refresh the data"
+git push https://vincenthuang282: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@github.com/vincenthuang282/StockAndTelegram.git --all
