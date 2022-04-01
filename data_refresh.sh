c465911aa46446ecc8229bd426ca4509ad5d53f7
Vincent Huang <vincenthuang282@gmail.com>
2022-04-01 01:55:17 +0000

    refresh the data

--- data_refresh.sh
+++ data_refresh.sh
@@ -10,4 +10,4 @@ git config --global user.email "vincenthuang282@gmail.com"
 git config --global user.name "Vincent Huang"
 git add *
 git commit -m "refresh the data"
-git push https://Vincenthuang282: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@github.com/Vincenthuang282/StockAndTelegram.git --all
+git push https://Vincenthuang282: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@github.com/Vincenthuang282/StockAndTelegram.git --all
