36987e90e14a730662c271d22d4c1ddb00d4bdeb
Vincent Huang <vincenthuang282@gmail.com>
2022-04-01 01:50:06 +0000

    refresh the data

--- data_refresh.sh
+++ data_refresh.sh
@@ -3,6 +3,11 @@
 python3 /home/vincenthuang282/StockAndTelegram/Fetch_Data_to_database.py 
 rm /home/vincenthuang282/StockAndTelegram/result_file/*
 python3 /home/vincenthuang282/StockAndTelegram/data_analyist.py 
+
+
+
+git config --global user.email "vincenthuang282@gmail.com"
+git config --global user.name "Vincent Huang"
 git add *
 git commit -m "refresh the data"
 git push https://Vincenthuang282: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@github.com/Vincenthuang282/StockAndTelegram.git --all
