c218281d3e4b67994aa030a82d111f3ef309950c
Vincent Huang <vincenthuang282@gmail.com>
2022-05-31 07:33:19 +0000

    dehashtag the cronjob

--- data_refresh.sh
+++ data_refresh.sh
@@ -6,8 +6,8 @@ python3 /home/vincenthuang282/StockAndTelegram/data_analyist.py
 
 
 
-git config --global user.email "vincenthuang282@gmail.com"
-git config --global user.name "Vincent Huang"
-git add *
-git commit -m "refresh the data"
-git push https://Vincenthuang282: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@github.com/Vincenthuang282/StockAndTelegram.git --all
+#git config --global user.email "vincenthuang282@gmail.com"
+#git config --global user.name "Vincent Huang"
+#git add *
+#git commit -m "refresh the data"
+#git push https://Vincenthuang282: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@github.com/Vincenthuang282/StockAndTelegram.git --all
